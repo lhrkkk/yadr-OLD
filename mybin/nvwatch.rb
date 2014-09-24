e5368154ec6897d0fe24be0079c35469597aadb9
lu haorui <luhaorui@gmail.com>
2014-09-24 16:36:36 +0800

    linode

--- mybin/nvwatch.rb
+++ mybin/nvwatch.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 # watch.rb by Brett Terpstra, 2011
-# Watch Notational Velocity notes folder for changes and 
+# Watch Notational Velocity notes folder for changes and
 # update a preview file for Marked with most recently-modified file contents
 # Requres that notes be stored as plain text and notes folder be customized below
 
 trap("SIGINT") { exit }
 
-watch_folder = "/Users/lhr/Library/Application\ Support/nvALT"
+watch_folder = "/Users/lhr/Marboo/"
 watch_types = ['txt','md']
-marked_note = File.expand_path("~/Marked Preview.md")
+marked_note = File.expand_path("~/Marboo/nvALT/Marked Preview.markdown")
 
 while true do # repeat infinitely
   files = []
@@ -18,7 +18,7 @@ while true do # repeat infinitely
   new_hash = files.collect {|f| [ f, File.stat(f).mtime.to_i ] } # create a hash of timestamps
   hash ||= new_hash
   diff_hash = new_hash - hash # check for changes
-  
+
   unless diff_hash.empty? # if changes were found in the timestamps
     hash = new_hash
     puts "changed"
@@ -29,6 +29,7 @@ while true do # repeat infinitely
     watch_note.puts note
     watch_note.close
   end
-  
+
   sleep 1
 end
+
